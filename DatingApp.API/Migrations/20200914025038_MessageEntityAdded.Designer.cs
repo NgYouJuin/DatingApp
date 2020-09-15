ced8b42b2df41186be6a6da04178833e64d794cf
NgYouJuin <ngyoujuin@yahoo.com>
2020-09-15 16:29:03 +0800

    End of section 16

--- DatingApp.API/Migrations/20200914025038_MessageEntityAdded.Designer.cs
+++ DatingApp.API/Migrations/20200914025038_MessageEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200914025038_MessageEntityAdded")]
+    partial class MessageEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -42,9 +44,6 @@ namespace DatingApp.API.Migrations
 
                     b.Property<DateTime?>("DateRead")
                         .HasColumnType("TEXT");
-
-                    b.Property<bool>("IsRead")
-                        .HasColumnType("INTEGER");
 
                     b.Property<DateTime>("MessageSent")
                         .HasColumnType("TEXT");
